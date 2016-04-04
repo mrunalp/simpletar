d61ffb93d3b160d1c2c33f02d75d443ff4eca085
Mrunal Patel <mrunalp@gmail.com>
2016-04-04 16:27:47 -0700

    Add support for untar to test program
    Signed-off-by: Mrunal Patel <mrunalp@gmail.com>

--- cmd/simpletar/main.go
+++ cmd/simpletar/main.go
@@ -15,6 +15,7 @@ func main() {
 	app.Usage = "simple tar/untar utility"
 	app.Commands = []cli.Command{
 		tarCommand,
+		untarCommand,
 	}
 
 	if err := app.Run(os.Args); err != nil {
@@ -39,6 +40,28 @@ var tarCommand = cli.Command{
 			log.Fatalf("--dest could not be empty")
 		}
 		if err := simpletar.Tar(source, dest); err != nil {
+			log.Fatalf("Failed to tar: %v", err)
+		}
+	},
+}
+
+var untarCommand = cli.Command{
+	Name:  "untar",
+	Usage: "untar a file",
+	Flags: []cli.Flag{
+		cli.StringFlag{Name: "source", Usage: "source tar file path"},
+		cli.StringFlag{Name: "dest", Usage: "destination directory path"},
+	},
+	Action: func(ctx *cli.Context) {
+		source := ctx.String("source")
+		dest := ctx.String("dest")
+		if source == "" {
+			log.Fatalf("--source could not be emptry")
+		}
+		if dest == "" {
+			log.Fatalf("--dest could not be empty")
+		}
+		if err := simpletar.Untar(source, dest); err != nil {
 			log.Fatalf("Failed to tar: %v", err)
 		}
 	},
